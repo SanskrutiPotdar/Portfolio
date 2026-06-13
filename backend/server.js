0ad95fa7efec33ef23902a5144feb264d17cf5b2
Sanskruti Potdar <SanskrutiPotdar>
2026-06-13 21:04:18 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,11 @@ const app = express()
 
 app.use(
   cors({
-    origin: 'http://localhost:5173'
+    origin: [
+    "http://localhost:5173",
+    "https://portfolio-frontend-91r3.onrender.com"
+    ],
+    credentials: true
   })
 )
 
